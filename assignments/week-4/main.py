1cffef45a8a9a755e9b066dd8ca676f6fb6f81ab
Shobha Murthy <shobha.murthy@carestream.com>
2022-10-07 01:25:39 -0500

    week_4

--- assignments/week-4/main.py
+++ assignments/week-4/main.py
@@ -10,10 +10,12 @@ app = FastAPI(title='Two Face')
 #to receive both (face-bokeh and face-emotion)
 @app.get("/", tags=["Health Check"])
 async def root():
-    response_string = {}
-
+    response_items = {}
+    
     response = requests.get('http://week-4_face_bokeh_1:8000')
-    response_string["face_bokeh"] = response.json()
+    response_items["face_bokeh"] = response.json()
     response = requests.get('http://week-4_face_emotion_1:8000')
-    response_string["face_emotion"] = response.json()
+    response_items["face_emotion"] = response.json()
+    line_format = '%s : %s'
+    response_string = "\n".join([line_format % (key, str(value)) for key, value in response_items.items()])
     return response_string
