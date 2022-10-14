81ab5041c2906f5991b3295053f2cb65e60aebeb
Shobha Murthy <shobha.murthy@carestream.com>
2022-10-13 21:22:41 -0500

    docker compose updates

--- assignments/week-5/main.py
+++ assignments/week-5/main.py
@@ -5,7 +5,35 @@ import requests
 # Let's generate a new FastAPI app
 # Generate a FastAPI instance called `app` with the title 'Triton Health Check'
 # https://fastapi.tiangolo.com/
+#We generate a new FastAPI app in the Prod environment
+#https://fastapi.tiangolo.com/
+app = FastAPI(title='triton')
+
+#Call your get function for a health Check
+#to receive both (face-bokeh and face-emotion)
+@app.get("/", tags=["Health Check"])
+async def root():
+    response_items = {}
+    
+    response = requests.get('http://face-bokeh-cntnr:8000')
+    response_items["face_bokeh"] = response.json()
+    response = requests.get('http://face-emotion-cntnr:8000')
+    response_items["face_emotion"] = response.json()
+    line_format = '%s : %s'
+    response_string = "\n".join([line_format % (key, str(value)) for key, value in response_items.items()])
+    return response_string
 
 
 #Call your get function for a health Check
 #to receive both (face-bokeh and face-emotion)
+@app.get("/", tags=["Health Check"])
+async def root():
+    response_items = {}
+    
+    response = requests.get('http://face-bokeh-cntnr:8000')
+    response_items["face_bokeh"] = response.json()
+    response = requests.get('http://face-emotion-cntnr:8000')
+    response_items["face_emotion"] = response.json()
+    line_format = '%s : %s'
+    response_string = "\n".join([line_format % (key, str(value)) for key, value in response_items.items()])
+    return response_string
