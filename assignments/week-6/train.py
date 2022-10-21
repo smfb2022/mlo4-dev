e258f907eeb7002418d8b7319853837704268dff
Shobha Murthy <shobha.murthy@carestream.com>
2022-10-20 19:51:25 -0500

    week6-gpu

--- assignments/week-6/train.py
+++ assignments/week-6/train.py
@@ -10,11 +10,14 @@ import mlflow.keras
 # Add mlflow logging
 
 
-input_dir =
-target_dir =
+input_dir = 'data'
+target_dir = 'target'
 img_size = (160, 160)
 num_classes = 3
 batch_size = 32
+
+mlflow.log_param("batch_size", batch_size)
+mlflow.log_prams("num_classes", num_classes)
 
 input_img_paths = sorted(
     [os.path.join(input_dir, fname) for fname in os.listdir(input_dir) if fname.endswith(".jpg")])
@@ -25,7 +28,7 @@ target_img_paths = sorted([
 ])
 
 # Free up RAM in case the model definition cells were run multiple times
-
+keras.backend.clear_session()
 
 # Build model
 model = get_model(img_size, num_classes)
@@ -46,10 +49,18 @@ val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_p
 model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy")
 
 # Set the keras callback using ModelCheckpoint to save the best results of the model
-callbacks =
+model_checkpoint = keras.callbacks.ModelCheckpoint(
+            "trained_model",
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=False,
+            mode="auto",
+            save_freq="epoch",
+        )
 
 # Train the model, doing validation at the end of each epoch.
 # Make sure to include the calllback for mlflow
 epochs = 15
-model.fit(train_gen, epochs=epochs, validation_data=val_gen)
+mlflow.keras.autolog()
+model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=[model_checkpoint])
 model.save('./segmentation/')
